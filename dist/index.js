f76574ccf884e1616655cda523968a3d5815343f
WhenisDinner <57213880+WhenisDinner@users.noreply.github.com>
2022-12-26 00:28:01 +0800

    Added directional light to scene

--- dist/index.js
+++ dist/index.js
@@ -1,45 +1,67 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
-const canvas = document.querySelector('.webgl')
-const scene = new THREE.Scene()
 
-const textureLoader = new THREE.TextureLoader()
-const myTexture = textureLoader.load('static/qt.jpg')
+class Application{
+    constructor(){
+        this.scene = new THREE.Scene()
+        
+        this.renderer = new THREE.WebGLRenderer()
+        this.renderer.setClearColor(0x000000, 1.0)
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+        this.renderer.shadowMap.enabled = true
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        
+        this.clock = new THREE.Clock()
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({
-    map: myTexture
-})
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000)
+        this.camera.position.set(15, 16, 12)
+        this.camera.lookAt(this.scene.position);
 
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+        //set up light
+        this.light = new THREE.DirectionalLight( 0xffffff, 1 );
+        this.light.position.set( 5, 8, 0 );
+        this.light.target.position.set(-3, -5, 5);
+        this.light.castShadow = true;
+        this.scene.add( this.light );
+        this.scene.add(this.light.target);
+        this.helper = new THREE.DirectionalLightHelper(this.light);
+        this.scene.add( this.helper );
 
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+        //load textures and meshes
+        this.textureLoader = new THREE.TextureLoader()
+        let texture = this.textureLoader.load('static/qt.jpg')
+        const geom_cube = new THREE.BoxGeometry(1, 1, 1)
+        const material = new THREE.MeshStandardMaterial({
+            map: texture
+        })
+        this.mesh = new THREE.Mesh(geom_cube, material)
+        this.mesh.castShadow = true
+        this.mesh.receiveShadow = true
+        this.scene.add(this.mesh)
+        
+        const geom_floor = new THREE.BoxGeometry(20, 1, 20)
+        let floor = new THREE.Mesh(geom_floor, material)
+        this.mesh.castShadow = true
+        floor.receiveShadow = true
+        this.scene.add(floor)
+        floor.position.y = -3
+        
+        //initial render
+        document.body.appendChild(this.renderer.domElement)
+        this.renderer.render(this.scene, this.camera)
+    }
 }
 
-const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 1000)
-camera.position.set(15, 16, 12)
-camera.lookAt(scene.position);
-
-const renderer = new THREE.WebGLRenderer()
-
-renderer.setClearColor(0x000000, 1.0);
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.shadowMap.enabled = true
-
-
-document.body.appendChild(renderer.domElement);
-renderer.render(scene, camera);
-const clock = new THREE.Clock()
+var app = new Application()
 
 function animate(){
-    const elapsedTime = clock.getElapsedTime()
-    mesh.position.y = Math.sin(elapsedTime) * 1
+    const elapsedTime = app.clock.getElapsedTime()
+    app.mesh.position.y = Math.sin(elapsedTime) * 1
+    app.mesh.rotateX(30*0.0003)
     requestAnimationFrame(animate)
-    mesh.rotateX(30*0.0003)
-    renderer.render(scene, camera)
+    app.renderer.render(app.scene, app.camera)
+    app.light.target.updateMatrixWorld();
+    app.helper.update();
 }
 
 animate()
