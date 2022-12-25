57dc3cc49580e53e4db1586f0a911a4bf8e4d06b
WhenisDinner <57213880+WhenisDinner@users.noreply.github.com>
2022-12-25 18:32:24 +0800

    Changed project setup

---
+++ dist/index.js
@@ -0,0 +1,38 @@
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
+const canvas = document.querySelector('.webgl')
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+})
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height, 0.1, 1000)
+camera.position.set(15, 16, 12)
+camera.lookAt(scene.position);
+
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setClearColor(0x000000, 1.0);
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.shadowMap.enabled = true
+
+
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
+
+function animate(){
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+}
+
+animate()
